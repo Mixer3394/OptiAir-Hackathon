3ca28c7d30c3725a6ae4e29b627724653c5c44cb
Kamil Legierski <leg.kamil@gmail.com>
2019-06-24 13:46:14 +0200

    Initial

---
+++ RestApi/RestApi/Contexts/OptiAirDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Contexts
+{
+    public class OptiAirDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=OptiAirDatabse.db");
+        }
+
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Measurement> Measurements { get; set; }
+    }
+}
---
+++ RestApi/RestApi/Models/Measurement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApi.Models
+{
+    public class Measurement
+    {
+        public int Id { get; set; }
+        public double PM1 { get; set; }
+        public double PM25 { get; set; }
+        public double PM10 { get; set; }
+        public double Temperature { get; set; }
+        public double Damp { get; set; }
+        public double Preasure { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public int DeviceId { get; set; }
+        public virtual Device Device { get; set; }
+    }
+}
